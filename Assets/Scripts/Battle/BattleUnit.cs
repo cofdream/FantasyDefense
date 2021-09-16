e7e0d5ff8af6e63bbecb9c50fef0ddf5614b2820
cofdream <cofdream@sina.com>
2021-09-16 10:02:52 +0800

    update

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleUnit : MonoBehaviour
 {
     [SerializeField] PetBase petBase;
     [SerializeField] int level;
+    [SerializeField] bool isPlayerUnit;
+    public Pet Pet { get; set; }
+
+    public void Setup()
+    {
+        Pet = new Pet(petBase, level);
+        GetComponent<Image>().sprite = isPlayerUnit ? Pet.PetBase.BackSprite : Pet.PetBase.FrontSprite;
+    }
 }
---
+++ Assets/Scripts/Battle/BttleSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BttleSystem : MonoBehaviour
+{
+    public BattleUnit battleUnit;
+    
+
+    //public  void ShowPlayer()
+    //{
+
+    //}
+
+    //public void Quit
+}
